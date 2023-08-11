e14b501841ca03cff4825ebcd4426d850ea4a4ce
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-08-11 14:02:03 -0300

    feat: create Movie model class

---
+++ Movies_API/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace Movies_API.Models;
+
+public class Movie {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int ReleaseYear { get; set; }
+    public int Duration { get; set; }
+    public HashSet<string> Genre { get; set; }
+
+    public override string ToString() {
+        string genresList = string.Join(", ", Genre);
+        return $"Titulo: {Title}\nAno: {ReleaseYear}\nDuração:{Duration} minutos\nGenero: {genresList}";
+    }
+}
