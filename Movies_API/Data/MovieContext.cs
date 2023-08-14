caa4db5857e5de39c2a0ae803ac9816986dbe032
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-08-14 10:22:20 -0300

    refactor: Change namespace usage

--- Movies_API/Data/MovieContext.cs
+++ Movies_API/Data/MovieContext.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Movies_API.Models;
 
-namespace Movies_API.Data {
-    public class MovieContext : DbContext {
-        public MovieContext(DbContextOptions<MovieContext> opts) : base(opts) { }
+namespace Movies_API.Data; 
+public class MovieContext : DbContext {
+    public MovieContext(DbContextOptions<MovieContext> opts) : base(opts) { }
 
-        public DbSet<Movie> Movies { get; set; }
-    }
+    public DbSet<Movie> Movies { get; set; }
 }
