9a0d6fbd880d4bc3821bf04571ccfa0090504188
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-08-14 10:20:51 -0300

    feat: Create profile to map DTO

---
+++ Movies_API/Profile/MovieProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using Movies_API.DTOs;
+using Movies_API.Models;
+
+namespace Movies_API.Profiles;
+
+public class MovieProfile : Profile {
+    public MovieProfile() {
+        CreateMap<CreateMovieDTO, Movie>();
+    }
+}
