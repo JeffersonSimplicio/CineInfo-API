05b1687df3fdb27ccb2c8bddac260e6eaa3679b7
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-08-16 15:32:20 -0300

    feat: create cinema controller

---
+++ CineInfo_API/Controllers/CinemaController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CineInfo_API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineInfo_API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CinemaController : Controller {
+    private CineInfoContext _dbContext;
+    private IMapper _mapper;
+
+    public CinemaController(CineInfoContext dbContext, IMapper mapper) {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+}
