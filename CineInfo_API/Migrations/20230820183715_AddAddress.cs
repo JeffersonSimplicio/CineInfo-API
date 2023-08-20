e21e58b6b0f05f25062c190709da2c2780bef98d
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-08-20 16:06:14 -0300

    feat: add 1:1 relationship between Cinemas and Addresses tables

---
+++ CineInfo_API/Migrations/20230820183715_AddAddress.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CineInfo_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "address_id",
+                table: "Cinemas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    neighborhood = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    street = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    number = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cinemas_address_id",
+                table: "Cinemas",
+                column: "address_id",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cinemas_Addresses_address_id",
+                table: "Cinemas",
+                column: "address_id",
+                principalTable: "Addresses",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cinemas_Addresses_address_id",
+                table: "Cinemas");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cinemas_address_id",
+                table: "Cinemas");
+
+            migrationBuilder.DropColumn(
+                name: "address_id",
+                table: "Cinemas");
+        }
+    }
+}
